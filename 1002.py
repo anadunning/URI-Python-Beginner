a71e5c9d635659144653d55289099de331721e2e
anadunning <anakpedrosa@hotmail.com>
2018-11-06 19:25:24 -0300

    First Python codes

---
+++ 1002.py
@@ -0,0 +1,5 @@
+R = float(input())
+pi = 3.14159
+A = pi * (R * R)
+AC = round(A, 4)
+print("A=", AC)
---
+++ 1113.py
@@ -0,0 +1,12 @@
+X = int(input())
+Y = int(input())
+
+while(X != Y):
+    if(X>Y):
+        print("Decrescente")
+    else:
+        print("Crescente")
+
+    X = int(input())
+    Y = int(input())
+
